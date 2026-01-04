04e0103d0a60a9c0f89413c672f06dc10652890a
ZainabM872 <zainabmajid2408@gmail.com>
2026-01-04 02:27:35 -0700

    native-helper-app: Implemented debouncing to reduce messaging overhead and cpu overload

--- native-helper/contextHelper.swift
+++ native-helper/contextHelper.swift
@@ -11,11 +11,32 @@ func getFrontmostApplicationName() -> String {
 // Separated into its own function so the timer callback stays lightweight.
 // FUTURE: Replace the print line with native messaging
 func printApp() {
-    print("app: " + (getFrontmostApplicationName() ?? ""))
+    print("app: " + (getFrontmostApplicationName()))
 }
 
-Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { 
-    _ in printApp()
+var lastActiveApp: String? = nil
+let debounceThreshold: TimeInterval = 1
+var currentAppStartTime = Date()
+var hasPrinted = false
+
+Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+    let currentApp = getFrontmostApplicationName()
+
+    // Check if the app has changed since last poll
+    if (currentApp != lastActiveApp) {
+        // A new app has been opened. Its stored in lastActiveApp, the timer is reset for debounce, and hasPrinted is set to false
+        lastActiveApp = currentApp
+        currentAppStartTime = Date()
+        hasPrinted = false
+    } else {
+        // The same app is still active. Check if it has been active long enough
+        // Only print if debounce threshold has passed and we haven’t printed yet
+        if (Date().timeIntervalSince(currentAppStartTime) > debounceThreshold && !hasPrinted) {
+            print("Frontmost app (debounced): \(currentApp)") // FUTURE: send this to Chrome via native messaging
+            currentAppStartTime = Date() 
+            hasPrinted = true 
+        }
+    }
 }
 
 RunLoop.current.run()
